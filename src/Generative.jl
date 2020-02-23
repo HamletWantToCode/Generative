54d5601beeee3f3769df20e3dbf8591b6d318cbb
hongbinren <hongbinrenscu@outlook.com>
2020-02-23 16:26:25 +0800

    Files generated by PkgTemplates

---
+++ src/Generative.jl
@@ -0,0 +1,5 @@
+module Generative
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Generative
+using Test
+
+@testset "Generative.jl" begin
+    # Write your own tests here.
+end
